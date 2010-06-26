cfd84878f9cf0037cafc4d7c146c89d12323c562
liuhui <liuhui@liuhui-desk.(none)>
2010-06-26 11:27:09 +0800

    将编译的源目录改成了text_zh

--- script/merge.rb
+++ script/merge.rb
@@ -2,7 +2,7 @@ desc 'Merge all of the texttile output into a single file for pdf conversion'
 
 task :merge do
   File.open('output/full_book.markdown', 'w+') do |f|
-    Dir["text/**/*.markdown"].sort.each do |path|
+    Dir["text_zh/**/*.markdown"].sort.each do |path|
       f << File.new(path).read + "\r\n"
     end
   end
